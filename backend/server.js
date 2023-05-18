2076cb5325766fde94ee68c35a9c66bba3c354d5
Behemoth39 <simme_jonsson@hotmail.com>
2023-05-18 12:27:56 +0200

    added backend

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,11 @@
-const express = require;
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Works?");
+});
+
+const PORT = 5000;
+
+app.listen(PORT);
