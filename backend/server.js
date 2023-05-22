9ffd1a0ce0252bd21a445a674bf75f882888b159
Behemoth39 <simme_jonsson@hotmail.com>
2023-05-22 19:14:11 +0200

    quick small api

--- backend/server.js
+++ backend/server.js
@@ -5,13 +5,25 @@ const axios = require("axios");
 dotenv.config({ path: "./config/.env" });
 
 const app = express();
+app.use(express.json());
 
-app.get("/api", (req, res) => {
-  res.send("Works?!");
+const URL = process.env.URL;
+
+app.get("/api/v1/food", async (req, res) => {
+  const { data } = await axios.get(URL);
+  res.status(200).json(data);
 });
 
-app.post("/api", (req, res) => {
-  res.send("Works?!");
+app.get("/api/v1/food/:id", async (req, res) => {
+  const id = req.params.id;
+  const { data } = await axios.get(URL + "/" + id);
+  res.status(200).json(data);
+});
+
+app.post("/api/v1/food", async (req, res) => {
+  const body = req.body;
+  const { data } = await axios.post(URL, body);
+  res.status(201).json(data);
 });
 
 const PORT = process.env.PORT;
