78c1d523f56f42b0c5afc07fd618dd0bdaee29a8
Behemoth39 <simme_jonsson@hotmail.com>
2023-05-22 13:14:11 +0200

    Added placeholder data

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,12 @@ const express = require("express");
 const dotenv = require("dotenv");
 const axios = require("axios");
 
-dotenv.config({ path: ".config/.env" });
+dotenv.config({ path: "./config/.env" });
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Works?");
+app.get("/api", (req, res) => {
+  res.send("Works?!");
 });
 
 const PORT = process.env.PORT;
